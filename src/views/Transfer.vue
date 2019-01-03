d6b749c2ce3f3f0d256cc7ebc6d02b603bc1708a
JingshengYe <jingsheng-ye@msyc.cc>
2019-01-03 18:12:55 +0800

    提交换乘功能测试

--- src/views/Transfer.vue
+++ src/views/Transfer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="transfer-amap-container">
-        <el-amap :zoom="zoom" :center="center" class="amap-demo" :plugin="plugin">
+        <el-amap :zoom="zoom" :center="center" class="amap-demo" :plugin="plugin" :amap-manager="amapManager">
         </el-amap>
         <div id="panel"></div>
     </div>
@@ -8,30 +8,39 @@
 
 <style>
     .transfer-amap-container {
-        height: 5rem;
+        height: 20rem;
     }
 </style>
 <script>
+    import {AMapManager} from "vue-amap"
+    let amapManager=new AMapManager();
     export default {
         data () {
             const self = this;
             return {
                 zoom: 13,
                 center: [121.5273285, 31.21515044],
+                amapManager,
                 plugin: [
                     {
                         pName: 'Transfer',
-                        // TODO 这里应该不是填这个的
-                        map: self,
+                        map: amapManager.getMap(),
                         city: '上海',
 //                        cityd:'目的城市',
                         panel: 'panel',
                         policy: 0,// 乘车策略，少时间0 少步行3 最少换乘2
                         events: {
                             init(o) {
-                                console.log(o);
                                 o.search([121.5273285, 31.21515044],[121.5873285, 31.25515044],function(status,result){
-                                    console.log(result);
+                                    if(status == 'complete'){
+                                        (new AMap.TransferRender()).autoRender({
+                                            data:result,
+                                            map:amapManager.getMap(),
+                                            panel:"panel"
+                                        });
+                                    }else{
+                                        console.log('公交路线数据查询失败' + result)
+                                    }
                                 });
                             }
                         }
